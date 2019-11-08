738eec21a7a0aa6d451fd884879a3f781500dc1a
hujishima takumi <hujishima.t425@gmail.com>
2019-11-08 23:13:19 +0900

    crait movingObjectScript

---
+++ VGA_Game_project_team4/Assets/Sclipt/movingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class movingObject : MonoBehaviour
+{
+    [SerializeField] float m_spead = 1f;
+    Rigidbody m_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_rb.velocity = Vector3.left * m_spead;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
